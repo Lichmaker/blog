bc87cf6104a0656afee761a197d896dfcff74f17
wuguozhang <lich.wu2014@gmail.com>
2019-08-08 20:49:35 +0800

    Revert "Revert "Merge branch 'feature/add-kan-ban-niang' into develop""
    This reverts commit cda68e8689d62234de0d1b41bdec2c9da24510d9.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- 加入看板娘 https://github.com/stevenjoezhang/live2d-widget -->
     <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css">
-    <script src="https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js"></script>
+    <script src="{{ URL::asset('live2d-widget/autoload.js') }}"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
