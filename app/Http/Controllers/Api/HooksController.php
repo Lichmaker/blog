cacd63d854f954ab6f6dfdcfc12a6da5b57614fc
wuguozhang <lich.wu2014@gmail.com>
2019-08-09 20:16:05 +0800

    :construction: 测试

--- app/Http/Controllers/Api/HooksController.php
+++ app/Http/Controllers/Api/HooksController.php
@@ -6,5 +6,7 @@ class HooksController extends ApiController
     public function github()
     {
         var_dump( \Request::json());
+
+        var_dump(json_decode(file_get_contents('php://input'), true));
     }
 }
